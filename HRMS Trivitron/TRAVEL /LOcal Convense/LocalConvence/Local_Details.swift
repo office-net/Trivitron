4d2647dbcf093e1f9092cd9dd34cf7af02a64e0b
Ankit Rana <ranaankitshishodia97@gmail.com>
2023-10-05 08:23:11 +0530

    Local convence is complete

---
+++ HRMS Trivitron/TRAVEL /LOcal Convense/LocalConvence/Local_Details.swift
@@ -0,0 +1,29 @@
+//
+//  Local_Details.swift
+//  HRMS Trivitron
+//
+//  Created by Netcommlabs on 29/09/23.
+//
+
+import UIKit
+
+class Local_Details: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
